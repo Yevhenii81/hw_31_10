d5f8af88c7a45989b3b99f8a6410296bf6a478ba
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2023-11-03 11:54:14 +0200

    Add files via upload

---
+++ hw_31_10/hw_31_10.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 35;
+
+    cout << (long long int) & a << "\n";
+    cout << (long long int) & b << "\n";
+
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+    int distance = (char*)ptr_b - (char*)ptr_a;
+
+    cout << distance << "\n";
+}
+
